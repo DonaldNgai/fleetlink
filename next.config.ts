63926f5279c1722b58286d8f9a36e44ac987aac7
Donald Ngai <donaldptngai@gmail.com>
2025-12-24 11:48:55 -0500

    Fixing for vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['chakra-ui', '@DonaldNgai/next-utils'],
+  transpilePackages: ['@DonaldNgai/chakra-ui', '@DonaldNgai/next-utils'],
   experimental: {
     clientSegmentCache: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Resolve TypeScript path aliases for webpack
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@ui': path.resolve(__dirname, 'packages/ui/src'),
+      '@utils': path.resolve(__dirname, 'packages/next-utils/src'),
+    };
+    return config;
   },
   images: {
     remotePatterns: [
