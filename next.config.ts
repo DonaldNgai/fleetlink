62334b5fdf246d65e4fcbbd87b334bc46c458881
Donald Ngai <donaldptngai@gmail.com>
2025-12-24 15:34:36 -0500

    Lots of transpile fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@DonaldNgai/chakra-ui', '@DonaldNgai/next-utils'],
+  transpilePackages: ['@DonaldNgai/next-utils'],
   experimental: {
     clientSegmentCache: true,
   },
