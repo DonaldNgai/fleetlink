2d7d05a124e169ff70231b0a43517dafc28680e1
Donald Ngai <donaldptngai@gmail.com>
2025-12-29 11:35:24 -0500

    Mvoing back to monorepo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@DonaldNgai/next-utils'],
+  transpilePackages: ['@DonaldNgai/next-utils', '@DonaldNgai/chakra-ui'],
   experimental: {
     clientSegmentCache: true,
   },
